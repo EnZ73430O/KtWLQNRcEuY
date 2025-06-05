43442970788c878c12cbb12a176219f1c7f3f82c
EnZ73430O <fliers-salutes.0x@icloud.com>
2025-06-05 07:08:37 +0200

    Add integration tests for the file's API endpoints

--- CWu/CX9tO/student-support_pr0qc6_1749067548.py
+++ CWu/CX9tO/student-support_pr0qc6_1749067548.py
@@ -18,7 +18,6 @@ def detect(img, cascade):
     rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv2.CASCADE_SCALE_IMAGE)
     if len(rects) == 0:
         return []
-    rects[:,2:] += rects[:,:2]
     return rects
 
 def draw_rects(img, rects, color):
